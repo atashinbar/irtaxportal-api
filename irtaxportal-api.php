6dc84f7869dfe9b0e3c118eeb369e9a14f3fc7b0
Ali Atashinbar <ali.atashinbar@gmail.com>
2023-09-26 15:44:37 +0330

    change author

--- irtaxportal-api.php
+++ irtaxportal-api.php
@@ -6,7 +6,7 @@
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      5.6
- * Author:            IRWP
+ * Author:            Moadianabzar
  * Author URI:        https://profiles.wordpress.org/irwp/
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
