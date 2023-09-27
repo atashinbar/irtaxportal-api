c484ae6a209d3c15522133be0e0a99236bd36c39
Ali Atashinbar <ali.atashinbar@gmail.com>
2023-09-27 19:35:32 +0330

    fix delete product

--- admin/services/Products.php
+++ admin/services/Products.php
@@ -65,8 +65,8 @@ class Products extends Registrerar {
                     // $value = $params;
                     if ( $update === 1 )
                     return static::create_response( 'با موفقیت اپدیت شد', 200 );
-                    else 
-                    return static::create_response( 'خطایی رخ داده است', 200 );
+                    else
+                    return static::create_response( 'خطایی رخ داده است', 403 );
                 }
             }
         }
@@ -81,36 +81,28 @@ class Products extends Registrerar {
 	 */
 	public static function delete_single_product( $request ) {
         $params	= $request->get_params();
+        $PId = $params[0];
 		$userId = get_current_user_id();
         if (!$userId) return static::create_response( 'شما مجوز لازم برای این کار را ندارید', 403 );
 
         global $wpdb;
         $tablename = $wpdb->prefix . "MA_products";
         $row = $wpdb->get_row($wpdb->prepare("SELECT * FROM `$tablename` WHERE user_id = %d", $userId), ARRAY_A);
-        return static::create_response($row['products'],200);
 
-        if (!is_array($row)) {
-            $user_id     = $userId;
-            $products[] = $params;
-            $products    = json_encode($products);
-            $sql = $wpdb->prepare("INSERT INTO `$tablename` (`user_id`, `products`) values (%d, %s)", $user_id, $products);
-            $wpdb->query($sql);
-        } else {
-            foreach ($row as $key => $value) {
-                if ($key === 'products'){
-                    $value = json_decode($value);
-                    $value[] = $params;
-                    $newValue = json_encode($value);
-                    $update = $wpdb->query($wpdb->prepare("UPDATE `$tablename` SET products='$newValue' WHERE user_id= %d", $userId));
-                    // $value = $params;
+        if (is_array($row)) {
+            $data = json_decode($row['products']);
+            foreach ($data as $key => $value) {
+                if ((int)$value->id === $PId) {
+                    unset($data[$key]);
+                    $newData = json_encode(array_values($data),JSON_UNESCAPED_UNICODE);;
+                    $update = $wpdb->query($wpdb->prepare("UPDATE `$tablename` SET products='$newData' WHERE user_id= %d", $userId));
                     if ( $update === 1 )
                     return static::create_response( 'با موفقیت اپدیت شد', 200 );
-                    else 
-                    return static::create_response( 'خطایی رخ داده است', 200 );
+                    else
+                    return static::create_response( 'خطایی رخ داده است', 403 );
                 }
             }
         }
-
-		return static::create_response( 'با موفقیت اپدیت شد', 200 );
+        return static::create_response( 'اطلاعات خواسته شده یافت نشد', 403 );
 	}
 }
