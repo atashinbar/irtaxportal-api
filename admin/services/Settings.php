655aa282a72debb0185f87c2498018cfb9f3d905
Sina Shamsizadeh <sina_shamsizadeh@hotmail.com>
2023-10-15 00:12:29 +0330

    Update Settings.php

--- admin/services/Settings.php
+++ admin/services/Settings.php
@@ -35,6 +35,14 @@ class Settings extends Registrerar {
 			$settings		= json_encode( $settings, JSON_UNESCAPED_UNICODE );
 			$sql			= $wpdb->prepare("INSERT INTO `$tablename` ( `user_id`, `settings` ) values (%d, %s)", $user_id, $settings);
 			$wpdb->query( $sql );
+
+			$message = 'شرکت جدید با نام ';
+			$message .= $params['name'] . ' ';
+			$message .= ' و کداقتصادی یا شماره‌ملی ';
+			$message .= $params['cod_eqtesadi'];
+			$message .= ' اضافه شد.';
+			return static::create_response( $message, 200 );
+
 		} else {
 			foreach ( $row as $key => $settings ) {
 
@@ -44,45 +52,67 @@ class Settings extends Registrerar {
 
 					foreach ( $settings as $key => $setting ) {
 
-						if ( $setting->cod_eqtesadi === $params['cod_eqtesadi'] ) {
+						if ( $setting->cod_eqtesadi === $params['cod_eqtesadi'] && $setting->cod_yekta === $params['cod_yekta'] ) {
 
-								$message = 'در گذشته شرکتی توسط شما با این مشخصات ثبت شده است نام شرکت ';
-								$message .= $setting->name;
-								$message .= ' می باشد.';
+							$message = 'در گذشته شرکتی توسط شما با این مشخصات ثبت شده است نام شرکت ';
+							$message .= $setting->name;
+							$message .= ' می باشد.';
+							return static::create_response( $message, 403 );
 
-								return static::create_response( $message, 403 );
+						} else if (
+							$setting->cod_eqtesadi === $params['cod_eqtesadi'] &&
+							$setting->cod_yekta !== $params['cod_yekta'] &&
+							$setting->name !== $params['name'] &&
+							$setting->license !== $params['license'] &&
+							$setting->private_key !== $params['private_key']
+						) {
+
+							$setting->cod_yekta = $params['cod_yekta'];
+							$setting->name = $params['name'];
+							$setting->license = $params['license'];
+							$setting->private_key = $params['private_key'];
+
+							break;
+
+						} else if (
+								$setting->cod_eqtesadi !== $params['cod_eqtesadi'] &&
+								$setting->cod_yekta !== $params['cod_yekta'] &&
+								$setting->name !== $params['name'] &&
+								$setting->license !== $params['license'] &&
+								$setting->private_key !== $params['private_key']
+						) {
+							$newValue	= json_encode( [$params], JSON_UNESCAPED_UNICODE );
+							$newValue	= json_decode( $newValue, false, 512, JSON_UNESCAPED_UNICODE );
+
+							break;
 						}
 
 					}
 
 
-					$newValue	= json_encode( [$params], JSON_UNESCAPED_UNICODE );
-					$newValue	= json_decode( $newValue, false, 512, JSON_UNESCAPED_UNICODE );
-					$newValue	= json_encode( array_merge( $newValue, $settings), JSON_UNESCAPED_UNICODE );
+
+					if ( isset( $newValue ) ) {
+						$newValue	= json_encode( array_merge( $newValue, $settings ), JSON_UNESCAPED_UNICODE );
+					} else {
+						$newValue	= json_encode( $settings, JSON_UNESCAPED_UNICODE );
+					}
 
 					$update		= $wpdb->query( $wpdb->prepare( "UPDATE `$tablename` SET settings='$newValue' WHERE user_id= %d", $userId ) );
 
 					if ( $update === 1 ) {
-						$message = 'شرکت جدید با نام ';
-						$message .= $params['name'] . ' ';
-						$message .= ' و کداقتصادی یا شماره‌ملی ';
-						$message .= $params['cod_eqtesadi'];
-						$message .= ' اضافه شد.';
+						$message = 'بروز رسانی با موفقیت انجام شد';
 						return static::create_response( $message, 200 );
 					} else {
 						return static::create_response( 'خطایی رخ داده است', 403 );
 					}
 
 				}
+
 			}
 		}
 
 
-		$message = 'شرکت جدید با نام ';
-		$message .= $params['name'] . ' ';
-		$message .= ' و کداقتصادی یا شماره‌ملی ';
-		$message .= $params['cod_eqtesadi'];
-		$message .= ' اضافه شد.';
+		$message = 'خطایی رخ داده است';
 		return static::create_response( $message, 200 );
 	}
 
@@ -137,7 +167,6 @@ class Settings extends Registrerar {
 	public static function get_company( $request ) {
 		$params			= $request->get_params();
 		$userId			= get_current_user_id();
-		$cod_eqtesadi	= sanitize_text_field( $params['cod_eqtesadi'] );
 
 
 		if ( ! $userId ) {
@@ -149,7 +178,7 @@ class Settings extends Registrerar {
 		$row		= $wpdb->get_row( $wpdb->prepare( "SELECT * FROM `$tablename` WHERE user_id = %d", $userId ), ARRAY_A );
 
 		if ( ! is_array( $row ) ) {
-            return [];
+           return static::create_response( ['settings'=>'{}'], 200 );
         }
 
 		return static::create_response( $row, 200 );
